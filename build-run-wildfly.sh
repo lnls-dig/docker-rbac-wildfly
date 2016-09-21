d467b739fca67a192b76b8a9e3aa3ad6760e4f9c
Lucas Russo <lerwys@gmail.com>
2016-09-21 09:40:32 -0300

    build-run-wildfly.sh: remove download-pre-reqs.sh as this is done inside Dockerfile

--- build-run-wildfly.sh
+++ build-run-wildfly.sh
@@ -10,9 +10,6 @@ LDAP_CREDENTIALS="$2"
 # Source env vars
 . ./env-vars.sh ${SERVICE}
 
-# Download prereqs
-./download-pre-reqs.sh ${SERVICE} ${LDAP_CREDENTIALS}
-
 # Build image
 ./build.sh ${SERVICE}
 
