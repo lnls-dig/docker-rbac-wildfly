96f53f035b9bef64300c64853d38b1576865f490
Lucas Russo <lerwys@gmail.com>
2016-09-21 09:38:24 -0300

    build.sh: remove obsolete deploy functionality
    This is done inside deploy-app.sh now.

--- build.sh
+++ build.sh
@@ -8,18 +8,4 @@ SERVICE="$1"
 
 . ./env-vars.sh ${SERVICE}
 
-# Copy files to correct locations, removing existing "wars" and "jars"
-rm -f deploy/*.war
-rm -f deploy/*.jar
-if [ "${SERVICE}" == "rbac" ]; then
-    cp ${RBAC_TARGET_WAR} deploy/
-    cp postgresql-${POSTGRES_VERSION}.jar deploy/
-elif [ "${SERVICE}" == "mgmt" ]; then
-    cp ${RBAC_TARGET_WAR} deploy/
-    cp postgresql-${POSTGRES_VERSION}.jar deploy/
-else
-    echo "Unsupported service"
-    exit 0
-fi
-
 docker build -t ${RBAC_DOCKER_ORG_NAME}/${RBAC_DOCKER_IMAGE_NAME} -f ${RBAC_DOCKERFILE_NAME} .
